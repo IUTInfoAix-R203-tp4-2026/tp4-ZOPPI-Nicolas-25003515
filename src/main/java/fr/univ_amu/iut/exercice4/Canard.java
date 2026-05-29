1822e2096a6ed3b78714b77530bdcfbe666bf4a7
Nicolas Zoppi <nicolas.zoppi@etu.univ-amu.fr>
2026-05-29 20:22:03 +0000

    feat(exercice4): termine l'exercice

---
+++ src/main/java/fr/univ_amu/iut/exercice4/Canard.java
@@ -0,0 +1,15 @@
+package fr.univ_amu.iut.exercice4;
+
+public class Canard extends Animal {
+
+  public Canard(String nom) {
+    super(nom);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public String faireDuBruit() {
+    // TODO Auto-generated method stub
+    return "Coin coin !";
+  }
+}
